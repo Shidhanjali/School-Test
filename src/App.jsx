937ec8ba421a7d251bf6ac9dc56b7f7dc9e9dcaa
Shidhanjali kumari <153732660+Shidhanjali@users.noreply.github.com>
2025-08-22 23:11:44 +0530

    jjd

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     <div className='text-2xl font-bold'>App is working
     <div>this is home page </div>
     <div>
-      this is about page 
+      this is about page chages made by branch 2
     </div>
     </div>
   )
