559193d2054706e5ef14aab54dcab366032be32e
Shidhanjali kumari <153732660+Shidhanjali@users.noreply.github.com>
2025-08-22 23:13:18 +0530

    ddfdf

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     <div className='text-2xl font-bold'>App is working
     <div>this is home page </div>
     <div>
-      this is about page 
+      this is about page this is changes made by branch 1
     </div>
     </div>
   )
